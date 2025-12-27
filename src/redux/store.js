cc289058dcf3bebbc2450315faa620707d36f8d4
KarthikeyanDurai24 <138101847+KarthikeyanDurai24@users.noreply.github.com>
2025-12-27 18:47:07 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
-import favoritesReducer from './favoritesSlice';
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import favoritesReducer from "./favoritesSlice";
 
+// Define myFood slice directly here
+const myFoodSlice = createSlice({
+  name: "myFood",
+  initialState: {
+    myRecipes: [], // stores custom recipes
+  },
+  reducers: {
+    addRecipe: (state, action) => {
+      state.myRecipes.push(action.payload);
+    },
+  },
+});
+
+// Export actions
+export const { addRecipe } = myFoodSlice.actions;
+
+// Configure store
 const store = configureStore({
   reducer: {
     favorites: favoritesReducer,
+    myFood: myFoodSlice.reducer, // include myFood slice
   },
 });
 
