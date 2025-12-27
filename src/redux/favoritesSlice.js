025eb7f545bc49863e64bce9da4540f6f86c5cee
KarthikeyanDurai24 <138101847+KarthikeyanDurai24@users.noreply.github.com>
2025-12-27 16:40:07 +0530

    Update favoritesSlice.js

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -1,14 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  favoriterecipes: [], // Updated to handle favorite articles
+  favoriterecipes: [], // Stores favorite recipes
 };
 
 const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-   
+    toggleFavorite: (state, action) => {
+      const recipe = action.payload;
+      const existingIndex = state.favoriterecipes.findIndex(
+        (fav) => fav.idMeal === recipe.idMeal // Use idMeal to uniquely identify recipes
+      );
+
+      if (existingIndex >= 0) {
+        // Recipe already exists, remove it
+        state.favoriterecipes.splice(existingIndex, 1);
+      } else {
+        // Recipe does not exist, add it
+        state.favoriterecipes.push(recipe);
+      }
+    },
   },
 });
 
